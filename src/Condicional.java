fbcd021075ae19ce79868d4e3d18b46625bc7aef
Tiago aparecido pereira <thyagoman@hotmail.com>
2023-08-09 12:32:13 -0300

    teste de carreira

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,18 +1,20 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Condicional {
 	public static void main(String[]args) {
 		Scanner input = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
 
-		int [] vetor = new int [4];
-		int soma = 0;
-		int media = 0;
+		double [] vetor = new double [4];
+		double soma = 0;
+		double media = 0;
 		int quantidadeNotas = 1;
 
 		for(int i = 0; i < vetor.length;i++) {
 			System.out.println("Digite o valor da nota "+quantidadeNotas++);
-			vetor[i] = input.nextInt(); 
+			vetor[i] = input.nextDouble(); 
 			soma += vetor[i];
 		}
 		media = soma/vetor.length;
@@ -25,19 +27,19 @@ public class Condicional {
 		
 		if(media < 5) {
 			
-			System.out.println("Aluno Reprovado NOTA FINAL = "+(double)media);
+			System.out.printf("Aluno Reprovado NOTA FINAL =  %.2f%n",media);
 		}
 		
-		else if(media > 5 && media< 7) {
+		else if(media >= 5 && media <= 7) {
 			
-			System.out.println("Aluno em Recuperação NOTA FINAL = "+(double)media);
+			System.out.printf("Aluno em Recuperação NOTA FINAL =  %.2f%n",media);
 		}
 		
-		else if(media > 7) {
+		else if(media >= 7) {
 			
-			System.out.println("Aluno Aprovado Parabéns NOTA FINAL = "+(double)media);
+			System.out.printf("Aluno Aprovado Parabéns NOTA FINAL = %.2f%n",media);
 		}
-		
+		input.close();
 	}
-
+	
 }
